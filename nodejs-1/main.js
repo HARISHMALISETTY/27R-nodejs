915d3eb4e0e26d93300a37eac447d1000e98ad65
HARISHMALISETTY <malisettyharish@gmail.com>
2025-02-20 09:36:50 +0530

    code

--- nodejs-1/main.js
+++ nodejs-1/main.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const url=require("url")
+const url = require("url");
 
 // const server = http.createServer((req, res) => {
 //   res.write("welcome to nodejs");
@@ -7,49 +7,105 @@ const url=require("url")
 // });
 // let obj = { name: "karthik", city: "hyd" };
 
+// const server = http.createServer((req, res) => {
+//   // console.log(req.url)
+//   // const parsedURl=url.parse(req.url,true);
+//   // console.log(parsedURl)
+//   // console.log(parsedURl.pathname)
+//   // const tickets=parsedURl.query.tickets;
+
+//  const parsedURl= url.parse(req.url,true)
+//  console.log(parsedURl)
+//  const tickets=parsedURl.query.tickets
+
+//    if (req.method == "GET") {
+//     if (parsedURl.pathname == "/CHAAVA") {
+//       res.writeHead(200, "ok", { "content-type": "application/json" });
+//       res.write(JSON.stringify({ "movie name": "CHAAVA", screen: "one",numberofTickets:tickets }));
+//       res.end();
+//     } else if (parsedURl.pathname == "/THANDEL") {
+//       res.writeHead(200, "ok", { "content-type": "application/json" });
+//       res.write(JSON.stringify({ "movie name": "THANDEL", screen: "two",numberofTickets:tickets }));
+//       res.end();
+//     } else if (parsedURl.pathname == "/SKYFORCE") {
+//       res.writeHead(200, "ok", { "content-type": "application/json" });
+//       res.write(JSON.stringify({ "movie name": "SKYFORCE", screen: "three",numberofTickets:tickets }));
+//       res.end();
+//     } else {
+//       res.writeHead(400, "not found", { "content-type": "application/json" });
+//       res.write(JSON.stringify({ message: "movie not showing now" }));
+//       res.end();
+//     }
+//   } else if (req.method == "POST") {
+//     res.end();
+//   } else if (req.method == "PUT") {
+//     res.end();
+//   } else if (req.method == "DELETE") {
+//     res.end();
+//   } else {
+//     res.end();
+//   }
+// });
+
 const server = http.createServer((req, res) => {
-  // console.log(req.url)
-  // const parsedURl=url.parse(req.url,true);
-  // console.log(parsedURl)
-  // console.log(parsedURl.pathname)
-  // const tickets=parsedURl.query.tickets;
-
- const parsedURl= url.parse(req.url,true)
- console.log(parsedURl)
- const tickets=parsedURl.query.tickets
-
-
-   if (req.method == "GET") {
-    if (parsedURl.pathname == "/CHAAVA") {
+  if (req.method == "GET") {
+    const parsedurl = url.parse(req.url, true);
+    console.log(parsedurl);
+    if (parsedurl.pathname == "/menu") {
       res.writeHead(200, "ok", { "content-type": "application/json" });
-      res.write(JSON.stringify({ "movie name": "CHAAVA", screen: "one",numberofTickets:tickets }));
+      res.write(
+        JSON.stringify({
+          veg: [{ panner: "150rs", gobi: "120rs" }],
+          nonveg: [{ chicken: "225rs", fish: "240rs" }],
+          message: "thankyou for order",
+        })
+      );
       res.end();
-    } else if (parsedURl.pathname == "/THANDEL") {
+    } else if (parsedurl.pathname == "/menu/veg") {
+      if (parsedurl.query.item == "panner") {
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            category: "veg",
+            item: parsedurl.query.item,
+            price: parsedurl.query.quantity * 150,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      } else if (parsedurl.query.item == "gobi") {
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            category: "veg",
+            item: parsedurl.query.item,
+            price: parsedurl.query.quantity * 120,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      } else {
+        res.writeHead(400, "not found", { "content-type": "application/json" });
+        res.write(JSON.stringify({ message: "item not available" }));
+        res.end();
+      }
+    } else if (parsedurl.pathname == "/menu/nonveg") {
       res.writeHead(200, "ok", { "content-type": "application/json" });
-      res.write(JSON.stringify({ "movie name": "THANDEL", screen: "two",numberofTickets:tickets }));
-      res.end();
-    } else if (parsedURl.pathname == "/SKYFORCE") {
-      res.writeHead(200, "ok", { "content-type": "application/json" });
-      res.write(JSON.stringify({ "movie name": "SKYFORCE", screen: "three",numberofTickets:tickets }));
-      res.end();
-    } else {
-      res.writeHead(400, "not found", { "content-type": "application/json" });
-      res.write(JSON.stringify({ message: "movie not showing now" }));
+      res.write(
+        JSON.stringify({ category: "nonveg", message: "thankyou for order" })
+      );
       res.end();
     }
-  } else if (req.method == "POST") {
+
     res.end();
-  } else if (req.method == "PUT") {
-    res.end();
-  } else if (req.method == "DELETE") {
+  } else if (req.method == "post") {
+    res.write("post request is not ready yet");
     res.end();
   } else {
-    res.end();
+    res.write("invalid request");
+    res.end("");
   }
 });
-
-server.listen("3101", () => {
+server.listen("3104", () => {
   console.log("server running");
 });
-
-
