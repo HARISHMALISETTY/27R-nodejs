7e65548aa7b8dc7b1ee824b524dc3fbcd27c0b0e
HARISHMALISETTY <malisettyharish@gmail.com>
2025-03-06 12:44:10 +0530

    bcrypt and env

---
+++ nodejs-1/express2.js
@@ -0,0 +1,9 @@
+const express=require("express");
+require("dotenv").config();
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.port)
+// const port=3250;
+
+// const db_password="db123"
+
+// console.log(port,db_password)
